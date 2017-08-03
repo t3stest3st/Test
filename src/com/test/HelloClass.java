2b7b2f663d92682633370a162d6ad992492077e3
Andrey <j2152325@mvrht.net>
2017-08-03 22:35:05 +0300

    First class on Git!

--- src/com/test/HelloClass.java
+++ src/com/test/HelloClass.java
@@ -2,6 +2,7 @@ package com.test;
 
 public class HelloClass {
     public static void main(String[] args) {
+        //2ez
         System.out.println("hay!");
         System.out.println("day!");
     }
