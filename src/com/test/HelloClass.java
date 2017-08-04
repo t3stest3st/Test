174bff2271cba8fb7befe9b56bfcca90feb1336f
Andrey <j2152325@mvrht.net>
2017-08-05 00:11:10 +0300

    Added: -ez

--- src/com/test/HelloClass.java
+++ src/com/test/HelloClass.java
@@ -2,7 +2,6 @@ package com.test;
 
 public class HelloClass {
     public static void main(String[] args) {
-        //2ez
         System.out.println("hay!");
         System.out.println("day!");
         System.out.println("kay!");
