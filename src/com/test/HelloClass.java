b945df20b119a34959f93006648d12f75a4bf7be
Andrey <j2152325@mvrht.net>
2017-08-06 20:34:37 +0300

    -day

--- src/com/test/HelloClass.java
+++ src/com/test/HelloClass.java
@@ -2,7 +2,6 @@ package com.test;
 
 public class HelloClass {
     public static void main(String[] args) {
-        System.out.println("hay!");
         System.out.println("day!");
         System.out.println("may!");
         System.out.println("Привет!");
