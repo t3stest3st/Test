128bf48b2bb4cbf995706dd3ecce880b9969fdbc
t3stest3st <30627452+t3stest3st@users.noreply.github.com>
2017-08-06 20:36:47 +0300

    -may

--- src/com/test/HelloClass.java
+++ src/com/test/HelloClass.java
@@ -3,7 +3,6 @@ package com.test;
 public class HelloClass {
     public static void main(String[] args) {
         System.out.println("day!");
-        System.out.println("may!");
         System.out.println("Привет!");
     }
 }
