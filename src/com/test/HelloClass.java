8562f1d3f29bc44f6f3a35271cdeef550ed62bd5
t3stest3st <30627452+t3stest3st@users.noreply.github.com>
2017-08-06 20:33:14 +0300

    Hotfix

--- src/com/test/HelloClass.java
+++ src/com/test/HelloClass.java
@@ -4,8 +4,6 @@ public class HelloClass {
     public static void main(String[] args) {
         System.out.println("hay!");
         System.out.println("day!");
-        System.out.println("kay!");
-        System.out.println("nay!");
         System.out.println("may!");
         System.out.println("Привет!");
     }
