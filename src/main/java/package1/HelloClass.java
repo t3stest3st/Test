88561203d3ae833af270c219102e5012f33ae29d
Andrey <j2152325@mvrht.net>
2017-08-10 01:35:17 +0300

    refactoring

--- src/main/java/package1/HelloClass.java
+++ src/main/java/package1/HelloClass.java
@@ -1,7 +1,8 @@
-package com.test;
+package main.java.package1;
 
 public class HelloClass {
     public static void main(String[] args) {
         System.out.println("Привет!");
+        System.out.println(MyMath.add(2,3));
     }
 }
