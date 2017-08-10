777cf85871630e26f36cd4f82934189ea3b042ca
Andrey <j2152325@mvrht.net>
2017-08-10 04:49:08 +0300

    Tests upgraded

--- src/main/java/com/testing/HelloClass.java
+++ src/main/java/com/testing/HelloClass.java
@@ -2,7 +2,7 @@ package com.testing;
 
 public class HelloClass {
     public static void main(String[] args) throws InterruptedException {
-        System.out.println("Привет!");
+        System.out.println("!");
         System.out.println(com.testing.MyMath.add(2,3));
         System.out.println(com.testing.MyMath.div(5,2));
     }
